405cd7c3173907ccc933b2ee1584f07e7d0e8e8c
nv.duenas <nv.duenas@uniandes.edu.co>
2021-11-06 23:12:57 -0500

    Added delete test

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -21,4 +21,17 @@ describe('TODOS page', () => {
       .click()
       .should('have.class', 'complete');
   });
+
+  it('should delete a TODO', () => {
+    cy.visit('todo')
+      .get('input')
+      .type('new TODO')
+      .get('button[type=submit]')
+      .click()
+      .get('.list-group-item > .btn')
+      .click()
+      .get('h1')
+      .should('contain', 'TodoApp ( 0 ) ');
+  });
+
 });
